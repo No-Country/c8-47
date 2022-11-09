289313759db04e4247b781d80fd6168b883cc9a2
Fer Ramirez <fer.eze.ram@gmail.com>
2022-11-09 17:58:53 -0300

    user schema added

---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["client", "admin"],
+      default: "client",
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const user = this;
+  const compare = await bcrypt.compare(candidatePassword, user.password);
+
+  return compare;
+};
+
+export default model("User", UserSchema);
