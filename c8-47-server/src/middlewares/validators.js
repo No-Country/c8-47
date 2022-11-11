a1a617b3a82817490d20b48da30da2a1e193d107
Fer Ramirez <fer.eze.ram@gmail.com>
2022-11-11 14:16:46 -0300

    move validations to its own .js

---
+++ c8-47-server/src/middlewares/validators.js
@@ -0,0 +1,39 @@
+import { body, check } from "express-validator";
+
+const emailValidation = body("email", "Ingresa un email válido")
+  .trim()
+  .notEmpty()
+  .isEmail()
+  .escape();
+
+const passwordValidation = body("password", "Ingresa una contraseña válida")
+  .trim()
+  .notEmpty()
+  .isLength({ min: 6 })
+  .escape();
+
+const repPasswordValidation = body(
+  "rep_password",
+  "Ingresa una contraseña válida"
+)
+  .trim()
+  .notEmpty()
+  .isLength({ min: 6 })
+  .escape()
+  .custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Las contraseñas deben coincidir");
+    } else {
+      return value;
+    }
+  });
+
+const signUpValidation = [
+  emailValidation,
+  passwordValidation,
+  repPasswordValidation,
+];
+
+const logInValidation = [emailValidation, passwordValidation];
+
+export { signUpValidation, logInValidation };
--- c8-47-server/src/routes/session.router.js
+++ c8-47-server/src/routes/session.router.js
@@ -2,42 +2,15 @@ import { Router } from "express";
 import { body } from "express-validator";
 
 import { signUp, logIn } from "../controllers/session.ctrl.js";
-
-const emailValidation = body("email", "Ingresa un email válido")
-  .trim()
-  .notEmpty()
-  .isEmail()
-  .escape();
-
-const passwordValidation = body("password", "Ingresa una contraseña válida")
-  .trim()
-  .notEmpty()
-  .isLength({ min: 6 })
-  .escape();
-
-const repPasswordValidation = body(
-  "rep_password",
-  "Ingresa una contraseña válida"
-)
-  .trim()
-  .notEmpty()
-  .isLength({ min: 6 })
-  .escape()
-  .custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Las contraseñas deben coincidir");
-    } else {
-      return value;
-    }
-  });
+import { signUpValidation, logInValidation } from "../middlewares/validators";
 
 const router = Router();
 
 router.post(
   "/signup", //!VOLVER A VER agregar require a toda la data de body
-  [emailValidation, passwordValidation, repPasswordValidation],
+  signUpValidation,
   signUp
 );
-router.post("/login", [emailValidation, passwordValidation], logIn);
+router.post("/login", logInValidation, logIn);
 
 export default router;
