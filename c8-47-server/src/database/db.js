744e49d20e66b13440f8afc25fff82e41b64718d
Fer Ramirez <fer.eze.ram@gmail.com>
2022-11-13 14:37:17 -0300

    add test env connection

--- c8-47-server/src/database/db.js
+++ c8-47-server/src/database/db.js
@@ -2,7 +2,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { connect } from "mongoose";
 
-const { DB_URL } = process.env;
+const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env;
+
+const connectionString = NODE_ENV === "test" ? MONGO_DB_URI_TEST : MONGO_DB_URI;
 
 const options = {
   useUnifiedTopology: true,
@@ -11,12 +13,16 @@ const options = {
 
 let mongoConnected = null;
 
+if (!connectionString) {
+  console.error("Importa el archivo .env");
+}
+
 const connectMongo = async (req, res, next) => {
   try {
     if (mongoConnected) {
       return mongoConnected;
     }
-    return connect(DB_URL, options, (err) => {
+    return connect(connectionString, options, (err) => {
       if (err) console.log(err);
       else {
         mongoConnected = true;
