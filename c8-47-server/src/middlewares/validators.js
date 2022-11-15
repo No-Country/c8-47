c5e972a416fb1678c17658db5204f965b7018bdf
Fer Ramirez <fer.eze.ram@gmail.com>
2022-11-15 14:55:25 -0300

    add strong name validation

--- c8-47-server/src/middlewares/validators.js
+++ c8-47-server/src/middlewares/validators.js
@@ -1,60 +1,64 @@
-import { check } from "express-validator";
+import { check } from 'express-validator';
 
-const emailValidation = check("email")
+const emailValidation = check('email')
   .trim()
   .notEmpty()
-  .withMessage("Ingresa tu email")
+  .withMessage('Ingresa tu email')
   .isEmail()
-  .withMessage("Ingresa un email válido")
+  .withMessage('Ingresa un email válido')
   .toLowerCase()
   .escape();
 
-const passwordValidation = check("password")
+const passwordValidation = check('password')
   .trim()
   .notEmpty()
-  .withMessage("Ingresa tu contraseña")
+  .withMessage('Ingresa tu contraseña')
   .isLength({ min: 8 })
-  .withMessage("La contraseña debe tener al menos 8 caracteres")
+  .withMessage('La contraseña debe tener al menos 8 caracteres')
   .isLength({ max: 32 })
-  .withMessage("La contraseña acepta como máximo 32 caracteres")
+  .withMessage('La contraseña acepta como máximo 32 caracteres')
   .escape();
 
-const repeatPasswordValidation = check("repeat_password")
+const repeatPasswordValidation = check('repeat_password')
   .trim()
   .notEmpty()
-  .withMessage("Repite tu contraseña")
+  .withMessage('Repite tu contraseña')
   .escape()
   .custom((value, { req }) => {
     if (value !== req.body.password) {
-      throw new Error("Las contraseñas no coinciden");
+      throw new Error('Las contraseñas no coinciden');
     } else {
       return value;
     }
   });
 
-const firstNameValidation = check("first_name")
+const firstNameValidation = check('first_name')
   .isString()
-  .withMessage("Ingresa un nombre válido")
+  .withMessage('Ingresa un nombre válido')
   .trim()
   .notEmpty()
-  .withMessage("Ingresa un nombre válido")
+  .withMessage('Ingresa un nombre válido')
   .isLength({ min: 2 })
-  .withMessage("El campo Nombre debe tener al menos 2 caracteres")
+  .withMessage('El campo Nombre debe tener al menos 2 caracteres')
   .isLength({ max: 24 })
-  .withMessage("El campo Nombre debe tener como máximo 24 caracteres")
+  .withMessage('El campo Nombre debe tener como máximo 24 caracteres')
+  .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/)
+  .withMessage('El campo Nombre solo acepta letras')
   .toLowerCase()
   .escape();
 
-const lastNameValidation = check("last_name")
+const lastNameValidation = check('last_name')
   .isString()
-  .withMessage("Ingresa un apellido válido")
+  .withMessage('Ingresa un apellido válido')
   .trim()
   .notEmpty()
-  .withMessage("Ingresa un apellido válido")
+  .withMessage('Ingresa un apellido válido')
   .isLength({ min: 2, max: 24 })
-  .withMessage("El campo Apellido debe tener al menos 2 caracteres")
+  .withMessage('El campo Apellido debe tener al menos 2 caracteres')
   .isLength({ max: 24 })
-  .withMessage("El campo Nombre debe tener como máximo 24 caracteres")
+  .withMessage('El campo Apellido debe tener como máximo 24 caracteres')
+  .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ]+$/)
+  .withMessage('El campo Apellido solo acepta letras')
   .toLowerCase()
   .escape();
 
