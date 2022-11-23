5fdd586f0d26aea544e89d4de2414b8739cf4c40
Fer Ramirez <fer.eze.ram@gmail.com>
2022-11-23 17:43:33 -0300

    Add Selector validations

---
+++ c8-47-server/src/middlewares/validations/selector.js
@@ -0,0 +1,38 @@
+import { check } from 'express-validator';
+
+import { checkValidations } from './checkValidations.js';
+
+const organizationValidation = check('organization')
+  .trim()
+  .notEmpty()
+  .withMessage('Ingresa la organización')
+  .isLength({ max: 64 })
+  .withMessage('La organización acepta como máximo 64 caracteres')
+  .escape();
+
+const nameValidation = check('name')
+  .trim()
+  .optional({ checkFalsy: true })
+  .isLength({ max: 64 })
+  .withMessage('El nombre acepta como máximo 64 caracteres')
+  .escape();
+
+const emailValidation = check('email')
+  .trim()
+  .notEmpty()
+  .withMessage('Ingresa el email')
+  .isEmail()
+  .withMessage('Ingresa un email válido')
+  .toLowerCase()
+  .isLength({ max: 64 })
+  .withMessage('El email debe tener como máximo 64 caracteres')
+  .escape();
+
+const jobValidation = [
+  nameValidation,
+  organizationValidation,
+  emailValidation,
+  checkValidations,
+];
+
+export { jobValidation };
