4965bd09a93c86538058ed77f12b8a79e3d525c2
Fer Ramirez <fer.eze.ram@gmail.com>
2022-12-08 09:59:34 -0300

    Add tag form

--- c8-47-client/src/Pages/Home.js
+++ c8-47-client/src/Pages/Home.js
@@ -1,18 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Outlet } from 'react-router-dom';
+
 import { Cv } from '../Components/Cv/Cv';
 import foto from '../Assets/Images/foto.png';
 import { AuthContext } from '../Context/AuthContext';
 import { DataContext } from '../Context/DataContext';
+// import customAxios from '../../Helpers/customAxios';
+
+import './Home.css';
 
 const Home = () => {
+  const [showInput, setShowInput] = useState(false);
   const { user } = useContext(AuthContext);
   const {
     state: { data },
   } = useContext(DataContext);
 
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
   console.log('user', user);
   console.log('data', data);
+
+  const handleShowInput = () => {
+    if (!showInput) setShowInput(true);
+  };
+
+  const submitForm = async (formData) => {
+    try {
+      // const { data } = await customAxios.post('/tag', formData);
+      console.log(formData);
+      // redirigir a /dashboard si el tag se crea con éxito
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const styling = {
     display: 'grid',
@@ -36,8 +62,44 @@ const Home = () => {
       </div>
       <div className=' w-full md:grid-cols-4	' style={styling}>
         <div className=''>
-          <div className='bg-gray-900 hover:bg-gray-800 w-[220px] h-[220px] text-white text-center grid place-content-center text-9xl cursor-pointer pb-1.5'>
-            +
+          <div
+            className='bg-gray-900 hover:bg-gray-800 w-[220px] h-[220px] text-white text-center grid place-content-center text-9xl cursor-pointer pb-1.5'
+            onClick={handleShowInput}
+          >
+            {showInput ? (
+              <form onSubmit={handleSubmit(submitForm)} className='tag-form'>
+                <input
+                  type='text'
+                  autoComplete='off'
+                  className='tag-input'
+                  {...register('name', {
+                    required: true,
+                    maxLength: 32,
+                  })}
+                />
+
+                {!errors.name && <p className='g-hidden-placeholder'>hidden</p>}
+                {errors.name?.type === 'required' && (
+                  <p className='g-error-input '>Ingrese el nombre</p>
+                )}
+                {errors.name?.type === 'maxLength' && (
+                  <p className='g-error-input '>
+                    El nombre acepta como máximo 32 caracteres
+                  </p>
+                )}
+
+                <div className='tag-buttons-container'>
+                  <input
+                    type='button'
+                    value='Cancelar'
+                    onClick={() => setShowInput(false)}
+                  />
+                  <input type='submit' value='Crear' />
+                </div>
+              </form>
+            ) : (
+              <span>+</span>
+            )}
           </div>
           <p className='mt-1 text-justify'>Crear un nuevo CV</p>
         </div>
